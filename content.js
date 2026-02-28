dd9ab5e3dc5abd996c60e73d4c40a8071252d77b
RegalTurtle <thysvanderschoot@gmail.com>
2026-02-28 15:30:56 -0500

    Separating feed and post logic

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 // console.log("I am running in:", window.location.href);
 
-const hide_posts = (post_list) => {
+const hide_posts_on_feed = (post_list) => {
   // post_list = [
   //   { id: "abc123", percent_ai: 0.5 },
   //   { id: "xyz999", percent_ai: 0.9 }
@@ -13,6 +13,21 @@ const hide_posts = (post_list) => {
     const shouldHide = post_list.some(p => p.id === post_id);
     if (shouldHide) {
       post.style.display = "none";
+    }
+  })
+};
+
+const hide_comments_on_post = (comment_list) => {
+  // comment_list = [
+  //   { id: "abc123", percent_ai: 0.5 },
+  //   { id: "xyz999", percent_ai: 0.9 }
+  // ]
+  const comments = document.querySelector("shreddit-comment-tree").querySelectorAll("shreddit-comment[thingid]");
+  comments.forEach(comment => {
+    const comment_id = comment.getAttribute("thingid");
+    const shouldHide = comment_list.some(c => c.id === comment_id);
+    if (shouldHide) {
+      comment.style.display = "none";
     }
   })
 };
