2317390886225991162bd59a1bbae744cda8d9e6
Aya Salama <asalama0204@gmail.com>
2026-02-28 19:29:50 -0500

    incomplete server

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 import requests
 import json
 import os
@@ -11,14 +11,66 @@ app = Flask(__name__)
 USER = os.environ.get('API_USER')
 SECRET = os.environ.get('API_SECRET')
 
-@app.route("/detect/image")
-def detect_image():
+def detect_post(post_id, content):
+    result = []
+
+    for image_url in content['images']:
+       percentage = detect_image(image_url)
+       if percentage >= 0:
+           result += [{post_id: percentage}]
+
+    for video_url in content['videos']:
+       percentage = detect_video(video_url)
+       if percentage >= 0:
+           result += [{post_id: percentage}]
+
+    for text in content['text']:
+       percentage = detect_text(text)
+       if percentage >= 0:
+           result += [{post_id: percentage}]
+
+    return result
+
+@app.route("/detect", methods=['POST'])
+def detect():
+    body = request.get_json()
+    output = []
+    
+    for post_id in body:
+        content = body[post_id]
+        output += detect_post(post_id, content)
+
+    return output
+
+def detect_image(url):
     params = {
-      'url': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F1261728%2Fpexels-photo-1261728.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-stywo-1261728.jpg%26fm%3Djpg&f=1&nofb=1&ipt=06fb9d00c40ca60ce334224892dbcfcb01d987735c682c2a677f82ed54d22676',
+      'url': url,
       'models': 'genai',
       'api_user': USER,
       'api_secret': SECRET, 
     }
-    r = requests.get('https://api.sightengine.com/1.0/check.json', params=params)
 
-    return json.loads(r.text)
+    r = requests.get('https://api.sightengine.com/1.0/check.json', params=params)
+    output = json.loads(r.text)
+
+    if 'ai_generated' in output:
+        return output['ai_generated']
+    return -1
+
+
+def detect_video(url):
+    params = {
+      'models': 'genai',
+      'api_user': USER,
+      'api_secret':SECRET 
+    }
+
+    files = {'media': url}
+    r = requests.post('https://api.sightengine.com/1.0/video/check-sync.json', files=files, data=params)
+    output = json.loads(r.text)
+
+    return output
+#    if 'ai_generated' in output:
+#        return output['ai_generated']
+#    return -1
+
