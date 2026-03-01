b9abd462a917c869f7108aaf605ef680ef24c90d
Aya Salama <asalama0204@gmail.com>
2026-02-28 21:12:57 -0500

    cache and use raw files
    refactor

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request
 import requests
 import json
 import os
@@ -11,23 +11,31 @@ app = Flask(__name__)
 USER = os.environ.get('API_USER')
 SECRET = os.environ.get('API_SECRET')
 
-def detect_post(post_id, content):
+CACHE = {}
+
+def detect_post(post_id):
+    if post_id in CACHE:
+        return CACHE[post_id]
+
     result = []
+    path = f'posts/{post_id}/'
 
-    for image_url in content['images']:
-       percentage = detect_image(image_url)
+    for entry in os.listdir(f'{path}/images'):
+       percentage = detect_image(f'{path}/images/{entry}')
        if percentage >= 0:
            result += [{post_id: percentage}]
 
-    for video_url in content['videos']:
-       percentage = detect_video(video_url)
+    for entry in os.listdir(f'{path}/videos'):
+       percentage = detect_video(f'{path}/videos/{entry}')
        if percentage >= 0:
            result += [{post_id: percentage}]
 
-    for text in content['text']:
-       percentage = detect_text(text)
-       if percentage >= 0:
-           result += [{post_id: percentage}]
+#    for text in content['text']:
+#       percentage = detect_text(text)
+#       if percentage >= 0:
+#           result += [{post_id: percentage}]
+
+    CACHE[post_id] = result
 
     return result
 
@@ -36,41 +44,43 @@ def detect():
     body = request.get_json()
     output = []
     
-    for post_id in body:
-        content = body[post_id]
-        output += detect_post(post_id, content)
+    for post_id in body['ids']:
+        output += detect_post(post_id)
 
     return output
 
-def detect_image(url):
+def detect_image(path):
     params = {
-      'url': url,
       'models': 'genai',
-      'api_user': USER,
-      'api_secret': SECRET, 
+      'api_user': USER, 
+      'api_secret': SECRET
     }
+    files = {'media': open(path, 'rb')}
+    r = requests.post('https://api.sightengine.com/1.0/check.json', files=files, data=params)
 
-    r = requests.get('https://api.sightengine.com/1.0/check.json', params=params)
     output = json.loads(r.text)
+    if 'type' in output:
+        return output['type']['ai_generated']
 
-    if 'ai_generated' in output:
-        return output['ai_generated']
     return -1
 
-
-def detect_video(url):
+def detect_video(path):
     params = {
       'models': 'genai',
       'api_user': USER,
       'api_secret':SECRET 
     }
 
-    files = {'media': url}
+    files = {'media': open(path, 'rb')}
     r = requests.post('https://api.sightengine.com/1.0/video/check-sync.json', files=files, data=params)
     output = json.loads(r.text)
 
-    return output
-#    if 'ai_generated' in output:
-#        return output['ai_generated']
-#    return -1
+    if 'data' not in output:
+        return -1
+
+    percentage = 0.0
+    for data in output['data']['frames']:
+        percentage = max(percentage, data['type']['ai_generated'])
+
+    return percentage
 
